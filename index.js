eb6c73f5a7795fa097181282202e04aa04b077a1
aldocesarmg <aldocesarmg@gmail.com>
2025-01-13 14:32:40 -0600

    Sudoku filled with zeros

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('hello');
+// let myArray = [1, 2, 3, 4, 4, 3];
+
+// Initializing array of depth 2 and filling with 0
+let sudoku = new Array(9).fill(new Array(9).fill(0));
+
+
+
+console.table(sudoku);
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
