735a5d83078b3d4c9966f8bc5179ac400be1c4da
aldocesarmg <aldocesarmg@gmail.com>
2025-01-24 15:08:27 -0600

    Row and col hover, element selected design working

--- ui_sudoku_game/src/components/SudokuTable.js
+++ ui_sudoku_game/src/components/SudokuTable.js
@@ -1,20 +1,50 @@
-import { Button, Popover } from 'flowbite-react';
+import { useState } from 'react';
+import { Button, Popover, Table } from 'flowbite-react';
+
+const defaultQuadrantCoordenades = [[0, 2], [3, 5], [6, 8]];
 
 export function SudokuTable(props) {
     let sudoku = props.sudoku;
-    
-    const sudokuRowIterator = sudoku.map((rowElement) => {
-        let sudokuColumnIterator = rowElement.map((columnElement) => {
+    const [hoveredRowElement, setHoveredRowElement] = useState(-1);
+    const [hoveredColumnElement, setHoveredColumnElement] = useState(-1);
+    const [selectedElement, setSelectedElement] = useState([-10, -10]);
+
+    const sudokuRowIterator = sudoku.map((rowElement, rowElementIndex) => {
+        let sudokuColumnIterator = rowElement.map((columnElement, columnElementIndex) => {
+            let baseStyle = 'font-medium text-center text-2xl border-2 border-solid text-slate-100';
+            let isOddCell = ((Math.floor(rowElementIndex/3)) + Math.floor(columnElementIndex/3)) % 2 === 1;
+            let isCurrentElementHovered = columnElementIndex === hoveredColumnElement || rowElementIndex === hoveredRowElement;
+            let isSelectedElement = (selectedElement[0] === rowElementIndex && selectedElement[1] === columnElementIndex) || columnElement === sudoku[selectedElement[0]][selectedElement[1]] && columnElement !== 0;
+            
+            if (isSelectedElement) baseStyle += ' bg-red-700';
+            else if (isCurrentElementHovered) baseStyle += ' bg-blue-950';
+            else if (isOddCell) baseStyle += ' bg-blue-900';
+            else baseStyle += ' bg-blue-800';
+
             return(
-            <td className='px-4 py-1 font-bold border-2 border-black border-collapse'>
-                {(columnElement === 0) ? '_' : columnElement}</td>);
+                <Table.Cell
+                    className={ baseStyle }
+                    onMouseEnter={() => setHoveredColumnElement(columnElementIndex)}
+                    onMouseLeave={() => setHoveredColumnElement(-1)}
+                    onClick={() => setSelectedElement([rowElementIndex, columnElementIndex])}>
+                    {(columnElement === 0) ? '·' : columnElement}
+                </Table.Cell>
+            );
         })
-        return(<tr>{sudokuColumnIterator}</tr>);
+        return(<Table.Row
+                onMouseEnter={() => setHoveredRowElement(rowElementIndex)}
+                onMouseLeave={() => setHoveredRowElement(-1)}>
+                {sudokuColumnIterator}
+            </Table.Row>);
     });
 
     return(
-        <table className='border-2 border-black border-collapse'>
-            {sudokuRowIterator}
-        </table>
+        <div className="overflow-x-auto">
+            <Table>
+                <Table.Body className="divide-y">
+                    {sudokuRowIterator}
+                </Table.Body>
+            </Table>
+        </div>
     )
 }
